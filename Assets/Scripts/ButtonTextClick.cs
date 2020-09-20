5edb046839aead332b8c93dc5dbcf8d310d231a4
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-09-20 22:08:56 +0700

    Main menu asset completion

---
+++ Assets/Scripts/ButtonTextClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTextClick : MonoBehaviour
+{
+    public RectTransform text;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = text.localPosition;
+        Debug.Log(position);
+    }
+
+    public void MoveDown()
+    {
+        text.localPosition = new Vector3(position.x, position.y - 6f, position.z);
+    }
+
+    public void MoveUp()
+    {
+        text.localPosition = new Vector3(position.x, position.y, position.z);
+    }
+}
