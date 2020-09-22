2bfa64ef253a2f8b1a854d48d4eafeed7816079b
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-09-22 22:41:08 +0700

    Fix comment and remove logging

--- Assets/Scripts/SongSelectEntrance.cs
+++ Assets/Scripts/SongSelectEntrance.cs
@@ -22,13 +22,11 @@ public class SongSelectEntrance : MonoBehaviour
     {
         if(animate == true)
         {
-            Debug.Log(speed);
-
             transform.Translate(Vector2.left * speed * Time.deltaTime);
             if(screenPanel.anchoredPosition.x < 360f) // interpolate speed after halfway
             {
                 float distance = Mathf.Min(360f, 180f); // interpolate between these distance
-                speed = Mathf.Lerp(0f, 1000f, screenPanel.anchoredPosition.x / distance); // 1200 to 0, interpolated by current x and distance between
+                speed = Mathf.Lerp(0f, 1000f, screenPanel.anchoredPosition.x / distance); // 1000 to 0, interpolated by current x and distance between
             }
 
             if (screenPanel.anchoredPosition.x <= 0)
