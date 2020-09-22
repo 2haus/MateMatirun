e86b59e86bc315bfc9ceb6e70aefaa2af4b2288a
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-09-22 21:14:53 +0700

    Switch scene to setting on click

---
+++ Assets/Scripts/MainMenuSettingButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSettingButton : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(EnterSettings);
+    }
+
+    void EnterSettings()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+}
