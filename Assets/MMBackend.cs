33a798963f6f76ba4b3e710331a36b68c4d80d5e
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-10-30 18:00:28 +0700

    Add NumberOfBackgrounds documentation

--- Assets/MMBackend.cs
+++ Assets/MMBackend.cs
@@ -263,6 +263,12 @@ namespace MMBackend
                 public static float jump = 15.84f;
             }
 
+            /// <summary>
+            /// Count number of backgrounds needed for the entire song.
+            /// </summary>
+            /// <param name="length">Total x transform length or audio length if audioLength is true.</param>
+            /// <param name="audioLength">Whether length is audio length.</param>
+            /// <returns></returns>
             public static int NumberOfBackgrounds(float length, bool audioLength)
             {
                 if (audioLength) length *= 2.5f;
