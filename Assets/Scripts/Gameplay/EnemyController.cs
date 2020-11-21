d4749840c7331bfb53a4a007f789a07751df6e7d
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-11-22 03:17:08 +0700

    Add animation event for enemy and controller

---
+++ Assets/Scripts/Gameplay/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator enemyAnimation;
+    public NoteLogic logic;
+
+    void Kill()
+    {
+        logic.status = false;
+    }
+
+    void Destroy()
+    {
+        logic.Destroy();
+    }
+}
