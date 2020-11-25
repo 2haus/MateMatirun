14c66f83e7e8852d2f3668c559d50670d9454f63
negpastalia <andykoesnaedi@gmail.com>
2020-11-25 08:04:48 +0700

    Add Highscore
    PlayerHighschool.cs
    HighscoreController.cs

---
+++ Assets/Scripts/Gameplay/HighscoreController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class HighscoreController {
+    public static void addSkor(int skor) {
+        int highscore1 = PlayerPrefs.GetInt("score1");
+        int highscore2 = PlayerPrefs.GetInt("score2");
+        int highscore3 = PlayerPrefs.GetInt("score3");
+        
+        if (skor > highscore1) {
+            highscore3 = highscore2;
+            highscore2 = highscore1;
+            highscore1 = skor;
+        } else if (skor > highscore2) {
+            highscore3 = highscore2;
+            highscore2 = skor;
+        } else if (skor > highscore3) {
+            highscore3 = skor;
+        }
+
+        PlayerPrefs.SetInt("score1", highscore1);
+        PlayerPrefs.SetInt("score2", highscore2);
+        PlayerPrefs.SetInt("score3", highscore3);
+    }
+}
---
+++ Assets/Scripts/Gameplay/PlayerHighschool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHighschool : MonoBehaviour
+{
+    public Text highScore1, highScore2, highScore3;
+    public Text scoreText;
+    int score = 0;
+    int playerHighScore;
+
+    void Update(){ 
+        playerHighScore = score;
+        scoreText.text = playerHighScore.ToString();
+        HighscoreController.addSkor(score);
+
+        //Show score
+        highScore1.text = PlayerPrefs.GetInt("score1").ToString();
+        highScore2.text = PlayerPrefs.GetInt("score2").ToString();
+        highScore3.text = PlayerPrefs.GetInt("score3").ToString();
+    }
+}
