072f2d2d934d03bc523266abba5577358567d4ec
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2020-11-26 10:32:16 +0700

    Move texts on clicking difficulty button

---
+++ Assets/Scripts/DifficultySelectionClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvalidArraySizeException : System.Exception
+{
+    public InvalidArraySizeException() : base(string.Format("Invalid array size (must be 2).")) { }
+}
+
+public class DifficultySelectionClick : MonoBehaviour
+{
+    public RectTransform[] text;
+    Vector3 mainPosition, choicePosition;
+
+    void Start()
+    {
+        if (text.Length != 2) throw new InvalidArraySizeException();
+        mainPosition = text[0].localPosition;
+        choicePosition = text[1].localPosition;
+
+        // Debug.Log($"{mainPosition.x}, {mainPosition.y}, {mainPosition.z}");
+        // Debug.Log($"{choicePosition.x}, {choicePosition.y}, {choicePosition.z}");
+    }
+
+    public void MoveDown()
+    {
+        text[0].localPosition = new Vector3(mainPosition.x, mainPosition.y - 12f, mainPosition.z);
+        text[1].localPosition = new Vector3(choicePosition.x, choicePosition.y - 12f, choicePosition.z);
+
+    }
+
+    public void MoveUp()
+    {
+        text[0].localPosition = mainPosition;
+        text[1].localPosition = choicePosition;
+    }
+}
