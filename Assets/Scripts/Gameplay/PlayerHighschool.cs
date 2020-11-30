61633099277edb9f9a80611bb6c0b9e60834e8c8
negpastalia <andykoesnaedi@gmail.com>
2020-11-30 21:43:24 +0700

    Fix Highscore system and minor changes

--- Assets/Scripts/Gameplay/PlayerHighschool.cs
+++ Assets/Scripts/Gameplay/PlayerHighschool.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
 
+public class HighScore{
+    public int score;
+    public HighScore(int score){
+        this.score = score;
+    }
+}
 public class PlayerHighschool : MonoBehaviour
 {
+    string[] songs = new string[]{
+        "Futatsu kageboushi",
+        "Strength",
+        "Taifunparedo",
+        "Zi lei yishan"
+    };
     public Text highScore1, highScore2, highScore3;
     public Text artistText, songText;
     public Text scoreText;
     public int score;
-
+    private string[] titleAndArtist;
+    string debugTitleSong;
+    string jsonTest;
     public Button retry, exit;
-
+    int id;
+    HighScore[] scores;
     void Start() {
         retry.onClick.AddListener(Retry);
         exit.onClick.AddListener(Exit);
@@ -21,17 +37,55 @@ public class PlayerHighschool : MonoBehaviour
         score = userScoreObj.GetComponent<UserScore>().score;
         artistText.text = userScoreObj.GetComponent<UserScore>().songArtist;
         songText.text = userScoreObj.GetComponent<UserScore>().songTitle;
+
+        debugTitleSong = artistText.text + " " + songText.text;
         Destroy(userScoreObj);
+
+        // HighScore[] testDemo = {
+        //     new HighScore(5), 
+        //     new HighScore(9),
+        //     new HighScore(7)
+        // };
+        // Debug.Log(JsonConvert.SerializeObject(testDemo));
+        for(int i = 0; i < songs.Length; i++){
+            if(songText.text == songs[i]){
+                id = i;
+                break;
+            }
+        }
+        if(!PlayerPrefs.HasKey($"{id}_score")){
+            HighScore[] tempScore = {
+                new HighScore(0),
+                new HighScore(0),
+                new HighScore(0)
+            };
+            PlayerPrefs.SetString($"{id}_score", JsonConvert.SerializeObject(tempScore));
+        }
+        scores = JsonConvert.DeserializeObject<HighScore[]>(PlayerPrefs.GetString($"{id}_score"));
         ApplyScore(score);
     }
 
     public void ApplyScore(int score){
         scoreText.text = score.ToString();
-        HighscoreController.addSkor(score);
+        Debug.Log(score);
+        Debug.Log(scores[0].score);
+        // HighscoreController.addSkor(score);
+        if(score > scores[0].score){
+            scores[2].score = scores[1].score;
+            scores[1].score = scores[0].score;
+            scores[0].score = score;
+        } else if (score > scores[1].score){
+            scores[2].score = scores[1].score;
+            scores[1].score = score;
+        } else if (score > scores[2].score){
+            scores[2].score = score;
+        }
 
-        highScore1.text = PlayerPrefs.GetInt("score1").ToString();
-        highScore2.text = PlayerPrefs.GetInt("score2").ToString();
-        highScore3.text = PlayerPrefs.GetInt("score3").ToString();
+        PlayerPrefs.SetString($"{id}_score", JsonConvert.SerializeObject(scores));
+
+        highScore1.text = scores[0].score.ToString();
+        highScore2.text = scores[1].score.ToString();
+        highScore3.text = scores[2].score.ToString();
     }
 
     void Retry()
