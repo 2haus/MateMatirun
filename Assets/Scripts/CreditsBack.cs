7a8cb72f293cf4380865a27c42e56f75b5ee5397
2haus <hansadithio@gmail.com>
2020-11-30 22:31:40 +0700

    Added Credit scene

---
+++ Assets/Scripts/CreditsBack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsBack : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ReturnToMenu);
+    }
+
+    void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
